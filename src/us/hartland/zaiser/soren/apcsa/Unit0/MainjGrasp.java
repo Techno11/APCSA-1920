5615405b44c2bff7052766eb5e108f3f107d4702
techno11 <soren.zaiser@gmail.com>
2019-08-23 07:59:32 -0400

    Update .gitignore, use jgrasp

---
+++ src/us/hartland/zaiser/soren/apcsa/Unit0/MainjGrasp.java
@@ -0,0 +1,9 @@
+public class MainjGrasp {
+
+    public static void main(String[] args) {
+	    char[] letters = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
+        for (char letter : letters) {
+            System.out.print(letter);
+        }
+    }
+}
