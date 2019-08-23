afc0911770e18fbf989f3e7a2feb9c04a36ebd2e
techno11 <soren.zaiser@gmail.com>
2019-08-23 07:27:48 -0400

    And then there was a hello world program
    mine is cool

---
+++ src/us/hartland/zaiser/soren/apcsa/Unit0/Main.java
@@ -0,0 +1,11 @@
+package us.hartland.zaiser.soren.apcsa.Unit0;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char[] letters = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd'};
+        for (char letter : letters) {
+            System.out.print(letter);
+        }
+    }
+}
